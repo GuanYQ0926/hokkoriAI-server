071231f9154f921ceef82217bcfd040b59b324e2
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-05 02:51:01 +0900

    audio test

--- server.py
+++ server.py
@@ -4,12 +4,8 @@ import io
 import os
 import werkzeug
 import tempfile
-import numpy as np
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
-import cv2
-from keras.models import model_from_json
-from keras.optimizers import Adam
 
 
 # sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
@@ -22,57 +18,6 @@ class TextResource(Resource):
     def get(self, parameters):
         print(str(parameters))
         return 'hello'
-
-
-class ImageResource(Resource):
-
-    def get(self, parameters):
-        # process image response
-        emotions = ['angry', 'disgust', 'fear', 'happy',
-                    'sad', 'surprise', 'neutral']
-        with open('./models/image/model.json', 'r') as f:
-            model_json = f.read()
-        model = model_from_json(model_json)
-        model.load_weights('./models/image/model.h5')
-        print('loaded mode')
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=Adam(), metrics=['accuracy'])
-        # process image
-        image_url = './files/images/temp.png'
-        img = cv2.imread(image_url, 0)
-        img = cv2.resize(img, (48, 48))
-        img = img.reshape((1, 48, 48, 1))
-        # predict
-        res = model.predict(img)[0]
-        emo = np.argmax(res)
-        return emotions[emo]
-
-    def post(self):
-        parse = reqparse.RequestParser()
-        parse.add_argument('images',
-                           type=werkzeug.datastructures.FileStorage,
-                           location='images')
-        data = parse.parse_args()
-        if data['images'] == '':
-            return {
-                'data': '',
-                'message': 'No file found',
-                'status': 'error'
-            }
-        images = data['images']
-        if images:
-            filename = 'temp.png'
-            images.save(os.path.join('./files/images', filename))
-            return {
-                'data': '',
-                'message': 'image saved',
-                'status': 'success'
-            }
-        return {
-            'data': '',
-            'message': 'Error when run',
-            'status': 'error'
-        }
 
 
 class AudioResource(Resource):
@@ -93,19 +38,8 @@ class AudioResource(Resource):
         return 'saved'
 
 
-class VideoResource(Resource):
-
-    def get(self, parameters):
-        pass
-
-    def post(self):
-        pass
-
-
 api.add_resource(TextResource, '/text/<parameters>')
-api.add_resource(ImageResource, '/image/<parameters>')
 api.add_resource(AudioResource, '/audio')
-api.add_resource(VideoResource, '/video/<parameters>')
 
 
 if __name__ == '__main__':
