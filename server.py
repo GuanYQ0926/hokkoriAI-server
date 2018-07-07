aad7fefcb4fc601267d0cbdedfeb5ce2f4f94f9c
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-07 20:38:49 +0900

    audio model

--- server.py
+++ server.py
@@ -2,13 +2,27 @@
 import sys
 import io
 import os
+import librosa
+import numpy as np
 from flask import Flask, request
 from flask_restful import Api, Resource
+from keras.models import model_from_json
+from keras.optimizers import Adadelta
 
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 app = Flask(__name__)
 api = Api(app)
+
+
+# load model in advance
+with open('./models/sounds/model.json', 'r') as f:
+    model_json = f.read()
+audio_model = model_from_json(model_json)
+audio_model.load_weights('./models/sounds/model.h5')
+print('model loaded')
+audio_model.compile(loss='categorical_crossentropy', optimizer=Adadelta(),
+                    metrics=['accuracy'])
 
 
 class TextResource(Resource):
@@ -21,10 +35,23 @@ class TextResource(Resource):
 class AudioResource(Resource):
 
     def post(self):
-        print('in audio processor')
+        # save audio file
+        file_path = './tempfiles/audio/temp.m4a'
         file = request.files['file']
-        file.save('./tempfiles/audio/temp.m4a')
-        return 'saved'
+        file.save(file_path)
+        # process
+        max_pad_len = 500
+        wave, sr = librosa.load(file_path, mono=True, sr=None)
+        mfcc = librosa.feature.mfcc(wave, sr=sr)
+        if mfcc.shape[1] > max_pad_len:
+            mfcc = mfcc[:, :max_pad_len]
+        else:
+            pad_width = max_pad_len - mfcc.shape[1]
+            mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)),
+                          mode='constant')
+        mfcc = mfcc.reshape(1, 20, max_pad_len, 1)
+        result = audio_model.predict(mfcc)[0]
+        return result
 
 
 api.add_resource(TextResource, '/text/<parameters>')
