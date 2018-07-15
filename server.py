d4442a87930b99571913c3e0375d90428ae4b436
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-15 12:00:30 +0900

    fix restful

--- server.py
+++ server.py
@@ -24,41 +24,73 @@ def text(parameters):
     return parameters
 
 
-class AudioResource(Resource):
+@app.route("/audio", methods=['POST', 'GET'])
+def post(self):
+    # load model in advance
+    with open('./models/sounds/model.json', 'r') as f:
+        model_json = f.read()
+    audio_model = model_from_json(model_json)
+    audio_model.load_weights('./models/sounds/model.h5')
+    print('model loaded')
+    audio_model.compile(loss='categorical_crossentropy',
+                        optimizer=Adadelta(), metrics=['accuracy'])
+    # save audio file
+    print('audio request in coming')
+    file_path = './tempfiles/audio/temp.mp3'
+    file = request.files['file']
+    file.save(file_path)
+    print('audio file is temporally saved')
+    # process
+    max_pad_len = 500
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    mfcc = librosa.feature.mfcc(wave, sr=sr)
+    if mfcc.shape[1] > max_pad_len:
+        mfcc = mfcc[:, :max_pad_len]
+    else:
+        pad_width = max_pad_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)),
+                      mode='constant')
+    mfcc = mfcc.reshape(1, 20, max_pad_len, 1)
+    res = audio_model.predict(mfcc)[0].tolist()
+    print('prediction end')
+    return jsonify(fussy=res[0], hungry=res[1], pain=res[2])
 
-    def post(self):
-        # load model in advance
-        with open('./models/sounds/model.json', 'r') as f:
-            model_json = f.read()
-        audio_model = model_from_json(model_json)
-        audio_model.load_weights('./models/sounds/model.h5')
-        print('model loaded')
-        audio_model.compile(loss='categorical_crossentropy',
-                            optimizer=Adadelta(), metrics=['accuracy'])
-        # save audio file
-        print('audio request in coming')
-        file_path = './tempfiles/audio/temp.mp3'
-        file = request.files['file']
-        file.save(file_path)
-        print('audio file is temporally saved')
-        # process
-        max_pad_len = 500
-        wave, sr = librosa.load(file_path, mono=True, sr=None)
-        mfcc = librosa.feature.mfcc(wave, sr=sr)
-        if mfcc.shape[1] > max_pad_len:
-            mfcc = mfcc[:, :max_pad_len]
-        else:
-            pad_width = max_pad_len - mfcc.shape[1]
-            mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)),
-                          mode='constant')
-        mfcc = mfcc.reshape(1, 20, max_pad_len, 1)
-        res = audio_model.predict(mfcc)[0].tolist()
-        print('prediction end')
-        return jsonify(fussy=res[0], hungry=res[1], pain=res[2])
+
+# class AudioResource(Resource):
+#
+#     def post(self):
+#         # load model in advance
+#         with open('./models/sounds/model.json', 'r') as f:
+#             model_json = f.read()
+#         audio_model = model_from_json(model_json)
+#         audio_model.load_weights('./models/sounds/model.h5')
+#         print('model loaded')
+#         audio_model.compile(loss='categorical_crossentropy',
+#                             optimizer=Adadelta(), metrics=['accuracy'])
+#         # save audio file
+#         print('audio request in coming')
+#         file_path = './tempfiles/audio/temp.mp3'
+#         file = request.files['file']
+#         file.save(file_path)
+#         print('audio file is temporally saved')
+#         # process
+#         max_pad_len = 500
+#         wave, sr = librosa.load(file_path, mono=True, sr=None)
+#         mfcc = librosa.feature.mfcc(wave, sr=sr)
+#         if mfcc.shape[1] > max_pad_len:
+#             mfcc = mfcc[:, :max_pad_len]
+#         else:
+#             pad_width = max_pad_len - mfcc.shape[1]
+#             mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)),
+#                           mode='constant')
+#         mfcc = mfcc.reshape(1, 20, max_pad_len, 1)
+#         res = audio_model.predict(mfcc)[0].tolist()
+#         print('prediction end')
+#         return jsonify(fussy=res[0], hungry=res[1], pain=res[2])
 
 
 # api.add_resource(TextResource, '/text/<parameters>')
-api.add_resource(AudioResource, '/audio')
+# api.add_resource(AudioResource, '/audio')
 
 
 if __name__ == '__main__':
